80b7ffa96bfaef13db38be2eb5a6a3d2e758ef34
Anna Popova <anna.popova201909@gmail.com>
2020-05-07 11:13:46 +0300

    added tests

--- src/test/java/ru/netology/domain/AuthTest.java
+++ src/test/java/ru/netology/domain/AuthTest.java
@@ -1,16 +1,22 @@
 package ru.netology.domain;
 
+import com.codeborne.selenide.Condition;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
-import static ru.netology.domain.PersonGenerator.Registration.generate;
 
 public class AuthTest {
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+    private static RegistrationData person;
+
+        private static RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
             .setAccept(ContentType.JSON)
@@ -18,61 +24,67 @@ public class AuthTest {
             .log(LogDetail.ALL)
             .build();
 
-//    @BeforeAll
-//    static void setUpAll() {
-//        // сам запрос
-//        given() // "дано"java -jar app-ibank.jar -P:profile=test
-//                .spec(requestSpec) // указываем, какую спецификацию используем
-//                .body(new RegistrationData(generate("en").getLogin(), generate("en").getPassword(), Status.ACTIVE)) // передаём в теле объект, который будет преобразован в JSON
-//                .when() // "когда"
-//                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-//                .then() // "тогда ожидаем"
-//                .statusCode(200); // код 200 OK
-//    }
-
-    @Test
-    void shouldRequestWithValidData() {
-        given()
-                .spec(requestSpec)
-                .body(new RegistrationData("vasya", "password"))
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
+    @BeforeAll
+    static void setUpAll() {
+        person = PersonGenerator.Registration.generate("en");
+        given() // "дано"java -jar app-ibank.jar -P:profile=test
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(person) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
     }
 
     @Test
-    void shouldNotRequestWithNotValidData() {
-        given()
-                .spec(requestSpec)
-                .body(new RegistrationData(generate("en").getLogin(), generate("en").getPassword(), Status.BLOCKED))
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(500); //
-    }
-
-    @Test
-    void shouldNotRequestWithoutLogin() {
-        given()
-                .spec(requestSpec)
-                .body(new RegistrationData(null, generate("en").getPassword(), Status.ACTIVE))
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(500);
+    void shouldRequestOrNotWithValidDataDependingOnStatus() {
+        open("http://localhost:9999");
+        $("input[name =\"login\"]").setValue(person.getLogin());
+        $("input[name=\"password\"]").setValue(person.getPassword());
+        $("button[type=\"button\"][data-test-id=\"action-login\"]").click();
+        if (person.getStatus().equals(Status.ACTIVE)) {
+            $(withText("Личный кабинет")).waitUntil(Condition.visible, 5000);
+        } else if ((person.getStatus().equals(Status.BLOCKED))) {
+            $("[data-test-id =\"error-notification\"]").waitUntil(Condition.visible, 5000);
+        }
     }
 
     @Test
     void shouldNotRequestWithoutPassword() {
-        given() //
-                .spec(requestSpec)
-                .body(new RegistrationData(generate("en").getLogin(), null,  Status.BLOCKED))
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(500);
+        open("http://localhost:9999");
+        $("input[name =\"login\"]").setValue(person.getLogin());
+        $("input[name=\"password\"]").setValue(" ");
+        $("button[type=\"button\"][data-test-id=\"action-login\"]").click();
+        $(withText("Поле обязательно для заполнения")).waitUntil(Condition.visible, 5000);
+    }
 
+    @Test
+    void shouldNotRequestWithoutLogin() {
+        open("http://localhost:9999");
+        $("input[name =\"login\"]").setValue(" ");
+        $("input[name=\"password\"]").setValue(person.getPassword());
+        $("button[type=\"button\"][data-test-id=\"action-login\"]").click();
+        $(withText("Поле обязательно для заполнения")).waitUntil(Condition.visible, 5000);
+    }
 
+    @Test
+    void shouldNotRequestWithValidPasswordButNotValidLogin() {
+        open("http://localhost:9999");
+        $("input[name =\"login\"]").setValue("Anna");
+        $("input[name=\"password\"]").setValue(person.getPassword());
+        $("button[type=\"button\"][data-test-id=\"action-login\"]").click();
+        $(withText("Ошибка")).waitUntil(Condition.visible, 5000);
+    }
+
+    @Test
+    void shouldNotRequestWithValidLoginButNotValidPassword() {
+        open("http://localhost:9999");
+        $("input[name =\"login\"]").setValue(person.getLogin());
+        $("input[name=\"password\"]").setValue("person");
+        $("button[type=\"button\"][data-test-id=\"action-login\"]").click();
+        $(withText("Ошибка")).waitUntil(Condition.visible, 5000);
     }
 }
+
+
+
--- src/test/java/ru/netology/domain/PersonGenerator.java
+++ src/test/java/ru/netology/domain/PersonGenerator.java
@@ -7,17 +7,25 @@ import java.util.Locale;
 public class PersonGenerator {
     private PersonGenerator(){}
 
-    public static class Registration {
+
+    public static  class Registration {
         private Registration(){}
+
 
         public static RegistrationData generate( String locale){
             Faker faker = new Faker(new Locale("en"));
+            double random = Math.random();
+            Status status;
+            if (random>=1){
+               status=Status.BLOCKED;
+            }  else {
+                status = Status.ACTIVE;
+            }
             return new RegistrationData(
                     faker.name().firstName(),
-                    faker. internet().password()
+                    faker. internet().password(),
+                    status
             );
-
-
         }
     }
 }
